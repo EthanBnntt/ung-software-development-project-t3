a72313541c75dcd3a8d34c4f25087a095e10df0b
Pamela/Ethan Bennett <95309712+EthanBnntt@users.noreply.github.com>
2024-07-09 18:34:32 -0400

    Prevent signing up over an existing account

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -11,6 +11,13 @@ export const userRouter = createTRPCRouter({
     signup: publicProcedure
         .input(z.object({ email: z.string().email(), password: z.string().min(8) }))
         .mutation(async ({ input }) => {
+            const existingUser = await db.user.findUnique({
+                where: { email: input.email },
+            });
+
+            if (existingUser)
+                throw new Error('That email has already been used');
+
             const hashedPassword = await hash(input.password, 10);
             const user = await db.user.create({
                 data: {
