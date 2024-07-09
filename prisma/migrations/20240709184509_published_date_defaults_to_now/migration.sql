05a1f45f817ef08ca27730e84acf0dcae168b9f3
Pamela/Ethan Bennett <95309712+EthanBnntt@users.noreply.github.com>
2024-07-09 18:34:06 -0400

    Migrations

---
+++ prisma/migrations/20240709184509_published_date_defaults_to_now/migration.sql
@@ -0,0 +1,20 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_DiscussionThread" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "publishedDate" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdById" TEXT NOT NULL,
+    "bookId" TEXT NOT NULL,
+    CONSTRAINT "DiscussionThread_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "DiscussionThread_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "Book" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_DiscussionThread" ("bookId", "createdAt", "createdById", "id", "publishedDate", "title", "updatedAt") SELECT "bookId", "createdAt", "createdById", "id", "publishedDate", "title", "updatedAt" FROM "DiscussionThread";
+DROP TABLE "DiscussionThread";
+ALTER TABLE "new_DiscussionThread" RENAME TO "DiscussionThread";
+CREATE INDEX "DiscussionThread_title_idx" ON "DiscussionThread"("title");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
