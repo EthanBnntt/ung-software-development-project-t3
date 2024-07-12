5448c189a81d9cb903e5bc2137e1047622e9314c
Pamela/Ethan Bennett <95309712+EthanBnntt@users.noreply.github.com>
2024-07-12 01:44:09 -0400

    Add genre tags to ViewBook component

--- src/app/_components/viewBook.tsx
+++ src/app/_components/viewBook.tsx
@@ -1,10 +1,18 @@
 "use client";
 
-import type { Book, Author } from '@prisma/client';
+import type { Book, Author, Genre } from '@prisma/client';
 
 import { api } from '~/trpc/react';
 import { BookThumbnail } from './bookThumbnail';
 import { ViewReviewsList } from './viewReviewsList';
+
+export function Pill({ children }: { children: React.ReactNode }) {
+    return (
+        <span className="inline-block bg-gray-200 rounded-full ml-3 px-3 py-1 text-sm font-semibold text-gray-700 mb-2">
+            {children}
+        </span>
+    );
+}
 
 export function ViewBook({ isbn, isAuthenticated }: { isbn: string, isAuthenticated: boolean }) {
     const bookQuery = api.book.getBookByISBN.useQuery({
@@ -16,7 +24,7 @@ export function ViewBook({ isbn, isAuthenticated }: { isbn: string, isAuthentica
     else if (bookQuery.error)
         return <div>Error: {bookQuery.error.message}</div>;
 
-    const book: (Book & { authors?: Author[] }) | undefined | null = bookQuery.data;
+    const book: (Book & { authors?: Author[], genres?: Genre[] }) | undefined | null = bookQuery.data;
 
     if (!book) {
         return <div>Book not found</div>;
@@ -36,6 +44,14 @@ export function ViewBook({ isbn, isAuthenticated }: { isbn: string, isAuthentica
                         book.description ? (<>
                             <strong>Description:</strong> {book.description}
                         </>) : <strong>Description not provided</strong>
+                    )}</p>
+                    <p className="text-lg mb-2">{(
+                        book.genres ? (<>
+                            <strong>Genres:</strong>
+                            <span className="pr-3">
+                                {book.genres.map((genre) => <Pill key={genre.name}>{genre.name}</Pill>)}
+                            </span>
+                        </>): <strong>Genres not provided</strong>
                     )}</p>
                 </div>
                 <div className="md:w-1/2 py-4">
