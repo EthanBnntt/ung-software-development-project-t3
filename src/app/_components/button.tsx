9142e9179cbe74387a485f61d446280284238535
Pamela/Ethan Bennett <95309712+EthanBnntt@users.noreply.github.com>
2024-07-16 00:15:30 -0400

    Improve Button styling

--- src/app/_components/button.tsx
+++ src/app/_components/button.tsx
@@ -2,9 +2,9 @@ import Link from 'next/link';
 
 export function Button({ children, href, type, onClick, ...props }: { children: React.ReactNode, type?: string, href?: string, onClick?: () => void }) {
     return (
-        <div className={"inline-flex items-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 border-2 cursor-pointer" +
-            (!type || type == "primary" ? " text-white bg-pink-500 hover:bg-pink-600 focus:ring-pink-300 border-pink-300 rounded-full" : "") +
-            (type == "secondary" ? " text-white bg-pink-300 hover:bg-pink-400 focus:ring-pink-200 border-2 border-pink-500 rounded-full" : "")}
+        <div className={"inline-flex items-center py-2 px-4 border-transparent rounded-md text-sm font-medium focus:outline-none cursor-pointer" +
+            (!type || type == "primary" ? " text-white bg-pink-500 hover:bg-pink-600 rounded-full" : "") +
+            (type == "secondary" ? " text-white bg-pink-300 hover:bg-pink-400 rounded-full" : "")}
             onClick={onClick}
             {...props}
         >
