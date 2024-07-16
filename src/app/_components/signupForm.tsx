55ee7e0e6303ad04464f5a4ddbcdf3cf1ce8064f
Pamela/Ethan Bennett <95309712+EthanBnntt@users.noreply.github.com>
2024-07-16 00:18:19 -0400

    Improve signup form styling

--- src/app/_components/signupForm.tsx
+++ src/app/_components/signupForm.tsx
@@ -5,6 +5,8 @@ import { z } from 'zod';
 import { api } from "~/trpc/react";
 import { signIn } from "next-auth/react";
 
+import { Button } from "./button";
+
 export function SignupForm() {
     const signup = api.user.signup.useMutation();
     const [error, setErrors] = useState<string[]>([]);
@@ -46,7 +48,7 @@ export function SignupForm() {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className="bg-gray">
             {error.length > 0 && (
                 <div className="mb-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
                     <strong className="font-bold">Error</strong>
@@ -58,30 +60,26 @@ export function SignupForm() {
                 </div>
             )}
             <div className="mb-4">
-                <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
+                <label className="block text-lg mb-1" htmlFor="body">Email:</label>
                 <input
                     type="email"
                     id="email"
                     name="email"
-                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    className="border rounded p-2 w-full"
                 />
             </div>
             <div className="mb-4">
-                <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+                <label className="block text-lg mb-1" htmlFor="body">Password:</label>
                 <input
                     type="password"
                     id="password"
                     name="password"
-                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    className="border rounded p-2 w-full"
                 />
             </div>
             <div className="flex items-center justify-between">
-                <button
-                    type="submit"
-                    className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    Sign Up
-                </button>
+                
+                <Button type="primary"><button type="submit">Sign Up</button></Button>
             </div>
         </form>
     )
