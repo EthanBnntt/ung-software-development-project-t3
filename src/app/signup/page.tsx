5424e7d6e7863dde8f1a2632f753467002d0a5e3
Pamela/Ethan Bennett <95309712+EthanBnntt@users.noreply.github.com>
2024-07-16 00:19:00 -0400

    More code to improve the signup form

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -6,9 +6,9 @@ import { SignupForm } from "~/app/_components/signupForm";
 export default async function SignupPage () {
     return (
         <HydrateClient>
-            <div className="min-h-screen flex items-center justify-center bg-gray-100">
-                <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
-                    <h2 className="text-2xl font-bold mb-6 text-center">Sign Up</h2>
+            <div className="min-h-screen flex items-center justify-center">
+                <div className="bg-white p-8 rounded w-full max-w-md border border-gray">
+                    <h2 className="text-2xl mb-2 text-center">Sign Up</h2>
                     <SignupForm></SignupForm>
                 </div>
             </div>
